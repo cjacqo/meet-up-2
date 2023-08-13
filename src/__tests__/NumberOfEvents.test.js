fe014c0fd3317b1a7344e9b56c666b3229babed2
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-08-13 13:23:13 -0700

    Test that user can remove default value of 32 and type in a value of 10 for numberofevents

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -3,6 +3,7 @@
 /* eslint-disable testing-library/no-render-in-setup */
 import { render } from '@testing-library/react'
 import NumberOfEvents from '../components/NumberOfEvents'
+import userEvent from '@testing-library/user-event'
 
 describe('<NumberOfEvents /> component', () => {
   let NumberOfEventsComponent
@@ -18,5 +19,12 @@ describe('<NumberOfEvents /> component', () => {
   test('determine if the default value of the textbox field is 32', () => {
     const textbox = NumberOfEventsComponent.queryByRole('textbox')
     expect(textbox).toHaveValue('32')
+  })
+
+  test('test if the user can change the number of events', async () => {
+    const user = userEvent.setup()
+    const textbox = NumberOfEventsComponent.queryByRole('textbox')
+    await user.type(textbox, '{backspace}{backspace}10')
+    expect(textbox).toHaveValue('10')
   })
 })
--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -3,9 +3,16 @@ import React, { useState } from 'react'
 const NumberOfEvents = () => {
   const [totalNumberOfEvents, setTotalNumberOfEvents] = useState(32)
 
+  const handleChange = e => {
+    setTotalNumberOfEvents(e.target.value)
+  }
+
   return (
     <div id='numberOfEvents'>
-      <input type='text' value={totalNumberOfEvents} />
+      <input
+        type='text'
+        value={totalNumberOfEvents}
+        onChange={handleChange} />
     </div>
   )
 }
