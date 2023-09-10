0002b86f399e9eb3153d87f4cb3858f5fbd2232c
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-09-09 22:05:27 -0700

    EventGenresChart now has labels with genre name and a percentage

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -32,6 +32,25 @@ const EventGenresChart = ({ events }) => {
     return data
   }
 
+  const renderCustomizedLabel = ({ cx, cy, midAngle, outerRadius, percent, index }) => {
+    const RADIAN = Math.PI / 180
+    const radius = outerRadius
+    const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07
+    const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07
+    return percent ? (
+      <text
+        x={x}
+        y={y}
+        fill='#8884D8'
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline='central'
+      >
+        {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+      </text>
+    ) : null
+  }
+
+  
   return (
     <ResponsiveContainer width='99%' height={400}>
       <PieChart>
@@ -40,7 +59,7 @@ const EventGenresChart = ({ events }) => {
           dataKey='value'
           fill='#8884D8'
           labelLine={false}
-          label
+          label={renderCustomizedLabel}
           outerRadius={130}
         />
       </PieChart>
