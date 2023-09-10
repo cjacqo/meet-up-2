339ab7346d6293db60a25bffb7f76b52fb9fd8ad
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-09-09 21:51:56 -0700

    Created EventGenresChart component

---
+++ src/components/EventGenresChart.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react'
+import {
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell
+} from 'recharts'
+
+const EventGenresChart = () => {}
+
+export default EventGenresChart
